d25efae29db5f29992e35ba5f876216d7e64e71f
Арчил Карчава <archil1997@gmail.com>
2018-08-02 21:52:34 +0500

    now using react-fontawesome for social links instead of simple img

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -1,25 +1,26 @@
 import React from 'react'
 import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-import vk from '../images/vk.svg'
-import instagram from '../images/instagram.svg'
+import { faVk, faInstagram } from '@fortawesome/free-brands-svg-icons'
 
 const Wrapper = styled.div`
   position: relative;
   vertical-align: middle;
+  list-style: none;
+  font-size: 28px;
+  ${props => props.centered && 'text-align: center'};
   margin: ${props => props.margin};
-  padding: ${props => props.padding};
-  img {
-    display: inline-block;
+  a {
+    display: inline;
     margin: 0 ${props => !!props.spaceBetween ? props.spaceBetween : '7px'};
-    height: 28px;
   }
 `
 
 const SocialLinks = props => (
   <Wrapper {...props}>
-    <a target="_blank" rel="noopener noreferrer" href="https://vk.com/"><img src={vk} alt="vk" /></a>
-    <a target="_blank" rel="noopener noreferrer" href="https://instagram.com/"><img src={instagram} alt="instagram" /></a>
+    <a target="_blank" rel="noopener noreferrer" href="https://vk.com/"><FontAwesomeIcon icon={faVk} /></a>
+    <a target="_blank" rel="noopener noreferrer" href="https://instagram.com/"><FontAwesomeIcon icon={faInstagram} /></a>
   </Wrapper>
 )
 
