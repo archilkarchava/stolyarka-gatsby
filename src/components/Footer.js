6ba1bda905ab04994742a09588c8388ee1b60bbf
Арчил Карчава <archil1997@gmail.com>
2018-08-27 10:42:54 +0500

    footer now also contains social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,34 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import SocialLinks from './SocialLinks'
+
 import setTextColor from '../utils/setTextColor'
 
 const Wrapper = styled.div`
   position: absolute;
   z-index: 2;
   width: 100%;
-  padding: 66px 0;
-  height: 19px;
+  padding: 50px 0;
   font-size: 16px;
+  p {
+    margin-bottom: 5px;
+  }
   background-color: ${props =>
     (props.primaryBg && props.theme.primary) ||
     (props.lightBg && props.theme.light) ||
     (props.darkBg && props.theme.dark) ||
     props.theme.dark};
-  color: ${props =>
-    setTextColor(
-      (props.primaryBg && props.theme.primary) ||
-        (props.lightBg && props.theme.light) ||
-        (props.darkBg && props.theme.dark) ||
-        props.theme.dark
-    )};
+  *,
+  a {
+    color: ${props =>
+      setTextColor(
+        (props.primaryBg && props.theme.primary) ||
+          (props.lightBg && props.theme.light) ||
+          (props.darkBg && props.theme.dark) ||
+          props.theme.dark
+      )};
+  }
   text-align: center;
 `
 
@@ -29,7 +36,15 @@ const Footer = props => {
   const { title: siteTitle, ...otherProps } = props
   return (
     <Wrapper {...otherProps}>
-      <p>&copy; {siteTitle} 2018. Все права защищены.</p>
+      <div>
+        <p>&copy; {siteTitle} 2018. Все права защищены.</p>
+        <SocialLinks
+          style={{
+            fontSize: '20px',
+            height: '24px',
+          }}
+        />
+      </div>
     </Wrapper>
   )
 }
